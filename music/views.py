7eb3233239fb4cde7212238a25a8e9803fe54a13
U-ENDAVA\cbicsabi <cbicsabi@EN61061.endava.net>
2016-07-28 11:48:06 +0300

    Initial Commit

---
+++ music/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Album
+
+
+def index(request):
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums': all_albums})
+
+
+def detail(request, album_id):
+    # album = Album.objects.get(pk=album_id)
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
